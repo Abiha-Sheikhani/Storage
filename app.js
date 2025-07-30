38c986cfd0111c28ebe7619e9e3870b2cd019edf
Abiha-Sheikhani <abihajawed345@gmail.com>
2025-07-30 11:54:59 +0500

    working

--- app.js
+++ app.js
@@ -6,8 +6,17 @@ const supabaseKey =
 const { createClient } = supabase;
 const client = createClient(supabaseUrl, supabaseKey)
 console.log(createClient)
-console.log(client);
+// console.log(client);
 
+
+// const path = window.location.pathname;
+
+// if (
+//   !path.includes("index.html") &&
+//   !path.includes("login.html") 
+// ) {
+//   window.location.href = "/index.html";
+// }
 
 //  signup code
 let signupBtn = document.getElementById('signUp-btn')
@@ -58,7 +67,7 @@ signupBtn &&
   }
 
 })
-
+google &&
 google.addEventListener('click', async () => {
   await client.auth.signInWithOAuth({
     provider: 'google',
@@ -69,7 +78,10 @@ google.addEventListener('click', async () => {
   })
 })
 
+// eye toggle
+
 let eye = document.getElementById('toggle-password')
+eye &&
 eye.addEventListener('click',()=>{
  eye.classList.toggle('disappear')
  if(eye.classList.contains('disappear')){
@@ -83,5 +95,138 @@ eye.addEventListener('click',()=>{
 })
 
 
-
+// login code
  
+let loginBtn = document.getElementById('login-btn')
+let email = document.getElementById('email')
+let password = document.getElementById('password')
+loginBtn &&
+  loginBtn.addEventListener('click', async ()=>{
+let email = document.getElementById('email').value
+let password = document.getElementById('password').value
+let timerInterval;
+
+  if ( email === "" && password === "") {
+    Swal.fire({
+      icon: "warning",
+      title: "Oops...",
+      text: "Please fill all the fields first!"
+    });
+  }
+  
+const { data, error } = await client.auth.signInWithPassword({
+                    email: email,
+                    password: password,
+                })
+console.log(data);
+
+
+   if (error) {
+    Swal.fire({
+      icon: "error",
+      title: "Login Failed",
+      text: error.message,
+    });
+  }
+  else {
+    Swal.fire({
+      icon: "success",
+      title: "Login Successfully!",
+      showConfirmButton: false,
+      timer: 1500
+    });
+
+   setTimeout(() => {
+      window.location.href = 'post.html';
+    }, 1500)
+
+
+
+  }
+  
+})
+
+
+// post functionality
+
+
+const post_button = document.getElementById('post-btn')
+ post_button &&
+post_button.addEventListener('click', async()=>{
+let post_title = document.getElementById('title').value
+let post_description = document.getElementById('description').value
+const { data : {user} } = await client.auth.getUser()
+console.log(user);
+if(post_title === "" && post_description === ""){
+  Swal.fire({
+      icon: "warning",
+      title: "Oops...",
+      text: "Please fill all the fields first!",
+    });
+}
+
+const { data,error } = await client
+  .from('Posts')
+  .insert({ 
+    user_id : user.id,
+    Title : post_title,
+    Description : post_description
+   })
+   if(error || null){
+    Swal.fire({
+      icon: "error",
+      title: "Post cannot be posted.",
+      text: error.message,
+    });
+   }
+   else{
+    // console.log(data);
+    
+     Swal.fire({
+      icon: "success",
+      title: "Post created successfully!",
+      showConfirmButton: false,
+      timer: 1500
+    })
+setTimeout(() => {
+      window.location.href = 'all-posts.html';
+    }, 1500)
+   }
+    document.getElementById('title').value = ""
+   document.getElementById('description').value = ""
+
+  
+  })
+ 
+
+
+
+if (window.location.pathname.includes("all-posts.html")) {
+  const readAllPosts = async () => {
+    const { data: fetchData, error: fetchError } = await client
+      .from('Posts')
+      .select();
+
+    if (fetchData) {
+      console.log(fetchData);
+        const container  = document.getElementById("container")
+    container.innerHTML = 
+    fetchData.map(
+      ({ user_id, Title, Description }) => 
+        `
+      <div class="card m-3 " style="width: 18rem;" id ="${user_id}">
+  <div class="card-body">
+    <h3 class="card-title">${Title}</h3>
+    <p class="card-text">${Description}</p>
+  </div>
+</div>
+      `
+      ).join("")
+    }
+    else {
+      console.error(fetchError.message);
+    }
+  };
+
+  readAllPosts(); 
+}
